89b63418d83978981a868bfe51370f59eb801a89
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2021-02-13 23:05:36 +0200

    Logging

--- entry.sh
+++ entry.sh
@@ -3,7 +3,8 @@
 # /bin/sleep inf
 dpkg-reconfigure debconf
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get -y install cron postfix
+apt-get update && apt-get -y install cron rsyslog mailutils postfix
+service rsyslog start
 service postfix start
 /usr/bin/crontab crontab.txt
 /usr/sbin/cron -f -l 8
